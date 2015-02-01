ada73f016c3ba8e3955760aff26277e0bdf52869
Stephen Parente <webmakersteve@gmail.com>
2015-02-01 13:59:48 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+require.config({
+    baseUrl: siteConfig.fullURL + 'vendor/components/',
+    paths: {
+        jquery: 'jquery/dist/jquery.min',
+        bootstrap_js: 'bootstrap-sass/assets/javascripts/bootstrap.min',
+        scroll_magic: 'ScrollMagic/js/jquery.scrollmagic.min',
+        jasny: 'jasny-bootstrap/dist/js/jasny-bootstrap.min'
+    },
+    shim: {
+      "bootstrap_js": ['jquery'],
+      "scroll_magic": ['jquery'],
+      "jasny": ['jquery', 'bootstrap_js'],
+      'scroll_magic': {
+        deps: ['jquery'],
+        exports: 'ScrollMagic'
+      }
+    }
+});
+
+require(['jquery', 'bootstrap_js', 'jasny'], function($) {
+
+  console.log($);
+  
+
+});
