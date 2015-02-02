6aaffc813159fcd1df0dcd52187fcfde77f7c5c5
Stephen Parente <webmakersteve@gmail.com>
2015-02-02 10:19:40 -0500

    Fixed baseurl call, holder in AMD, and lorem ipsum

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ require.config({
         jquery: 'jquery/dist/jquery.min',
         bootstrap_js: 'bootstrap-sass/assets/javascripts/bootstrap.min',
         scroll_magic: 'ScrollMagic/js/jquery.scrollmagic.min',
-        jasny: 'jasny-bootstrap/dist/js/jasny-bootstrap.min'
+        jasny: 'jasny-bootstrap/dist/js/jasny-bootstrap.min',
+        holder: 'https://cdnjs.cloudflare.com/ajax/libs/holder/2.4.1/holder'
     },
     shim: {
-      "bootstrap_js": ['jquery'],
+      "bootstrap_js": ['jquery', 'holder'],
       "scroll_magic": ['jquery'],
       "jasny": ['jquery', 'bootstrap_js'],
       'scroll_magic': {
